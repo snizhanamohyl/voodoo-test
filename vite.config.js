16a0933a3a87a1a0b5674dd043f2653afa0e65b8
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-14 16:24:42 +0300

    change: build outDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import handlebars from "vite-plugin-handlebars";
 
 export default {
   root: "src",
+  build: {
+    outDir: "../dist",
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials"),

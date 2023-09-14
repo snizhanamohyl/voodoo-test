f9749dc21bdff4d150b38952b0c41057cb8b8582
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-14 16:19:56 +0300

    add: src folder

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default {
+  root: "src",
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "partials"),
+      partialDirectory: resolve(__dirname, "src/partials"),
     }),
   ],
 };

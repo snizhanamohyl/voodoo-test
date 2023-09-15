c206cde839109bc7ce25793ca4ee7ac7fbc83715
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-15 14:37:31 +0300

    add: hover

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { resolve } from "path";
-import handlebars from "vite-plugin-handlebars";
-
 export default {
   root: "src",
   build: {
     outDir: "../dist",
   },
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, "src/partials"),
-    }),
-  ],
+  // plugins: [
+  //   handlebars({
+  //     partialDirectory: resolve(__dirname, "src", "hbs", "partials"),
+  //     // compileOptions: {
+  //     //   // Example config option: avoid auto-indenting partials
+  //     //   preventIndent: false,
+  //     // },
+  //   }),
+  // ],
 };

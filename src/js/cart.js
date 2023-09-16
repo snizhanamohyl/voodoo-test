d5abbe7e03e6d826989f29c92ed6884cc2e5ab8d
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-16 12:09:27 +0300

    add: changing quantity, counting total cost

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,12 @@
 const productListEl = document.getElementById("card-list");
 const cartListEl = document.getElementById("cart-list");
 
-const getProductFromLS = () => {
+const changeQuatityTypes = {
+  INCREASE: "INCREASE",
+  DECREASE: "DECREASE",
+};
+
+const getProductsFromLS = () => {
   const products = localStorage.getItem("productsInCart")
     ? JSON.parse(localStorage.getItem("productsInCart"))
     : [];
@@ -9,20 +14,39 @@ const getProductFromLS = () => {
   return products;
 };
 
+const normalizeCost = (cost) => {
+  return Number.isInteger(cost)
+    ? `${cost}.00`
+    : (Math.ceil(cost * 100) / 100).toFixed(2);
+};
+
+const countTotalCost = () => {
+  const productsInCart = getProductsFromLS();
+
+  const totalCost = productsInCart.reduce(
+    (acc, product) => (acc += Number(product.price) * Number(product.quantity)),
+    0
+  );
+
+  document.querySelector("[data-cost]").innerText = `${normalizeCost(
+    totalCost
+  )} KR.`;
+};
+
 const addProductToLS = (product) => {
-  const productsInCart = getProductFromLS();
+  const productsInCart = getProductsFromLS();
 
   if (productsInCart.find((cartProduct) => cartProduct.id === product.id))
     return;
 
   localStorage.setItem(
     "productsInCart",
-    JSON.stringify([...productsInCart, product])
+    JSON.stringify([...productsInCart, { ...product, quantity: 1 }])
   );
 };
 
 const removeProductFromLS = (productId) => {
-  const productsInCart = getProductFromLS();
+  const productsInCart = getProductsFromLS();
 
   const productIndex = productsInCart.findIndex(
     (product) => product.id === productId
@@ -33,18 +57,56 @@ const removeProductFromLS = (productId) => {
   localStorage.setItem("productsInCart", JSON.stringify(productsInCart));
 };
 
-const createCartItemMarkup = ({ id, title, img, price }) => {
+const changeProductInLS = (productId, dataToUpdate) => {
+  const productsInCart = getProductsFromLS();
+
+  const updatedProducts = productsInCart.map((product) =>
+    product.id === productId ? { ...product, ...dataToUpdate } : product
+  );
+
+  localStorage.setItem("productsInCart", JSON.stringify(updatedProducts));
+};
+
+const changeQuatity = (e, type) => {
+  const card = e.target.closest("[data-id]");
+  const productId = card.dataset.id;
+  const currentQuantity = Number(
+    card.querySelector("[data-quantity]").innerText
+  );
+
+  const newQuantity =
+    type === changeQuatityTypes.INCREASE
+      ? currentQuantity + 1
+      : currentQuantity - 1;
+
+  if (newQuantity < 1) return;
+
+  if (newQuantity === 1) {
+    card.querySelector("[data-minus").classList.add("text-modalHover");
+  } else {
+    card.querySelector("[data-minus").classList.remove("text-modalHover");
+  }
+
+  changeProductInLS(productId, { quantity: newQuantity });
+
+  card.querySelector("[data-quantity]").innerText = newQuantity;
+  countTotalCost();
+};
+
+const createCartItemMarkup = ({ id, title, img, price, quantity }) => {
   return `<li data-id="${id}" class="flex gap-[18px]">
           <img class="w-[74px] h-[74px] border border-secondary bg-secondary object-cover rounded-sm" width="74" height="74" src="${img}" alt="${title}">
           <div class="flex flex-col self-stretch justify-between grow">
             <p>${title}</p>
             <p>${price} KR.</p>
             <div class="flex items-center">
-              <button class="w-5 h-5 flex items-center justify-center transition-colors rounded-full p-3 hover:text-modalHover focus:text-modalHover">
+              <button data-minus class="${
+                quantity === 1 ? "text-modalHover" : ""
+              } w-5 h-5 flex items-center justify-center transition-colors rounded-full p-3 hover:text-modalHover focus-visible:text-modalHover">
                 -
               </button>
-              <p class="w-5 h-5 flex items-center justify-center">1</p>
-              <button class="w-5 h-5 flex items-center justify-center transition-colors rounded-full p-3 text-secondary hover:text-modalHover focus:text-modalHover">
+              <p data-quantity class="w-5 h-5 flex items-center justify-center">${quantity}</p>
+              <button data-plus class="w-5 h-5 flex items-center justify-center transition-colors rounded-full p-3 text-secondary hover:text-modalHover focus-visible:text-modalHover">
                 +
               </button>
             </div>
@@ -64,8 +126,24 @@ const addListenerToDeleteBtns = () => {
   deleteBtnsElems.forEach((btn) => btn.addEventListener("click", onDelete));
 };
 
+const addListenersToChangeQuantityBtns = () => {
+  const plusBtnsElems = document.querySelectorAll("[data-plus]");
+  const minusBtnsElems = document.querySelectorAll("[data-minus]");
+
+  plusBtnsElems.forEach((btn) =>
+    btn.addEventListener("click", (e) =>
+      changeQuatity(e, changeQuatityTypes.INCREASE)
+    )
+  );
+  minusBtnsElems.forEach((btn) =>
+    btn.addEventListener("click", (e) =>
+      changeQuatity(e, changeQuatityTypes.DECREASE)
+    )
+  );
+};
+
 const renderCartItems = () => {
-  const productsInCart = getProductFromLS();
+  const productsInCart = getProductsFromLS();
 
   const markup = productsInCart.reduce((acc, product) => {
     return (acc += createCartItemMarkup(product));
@@ -73,6 +151,7 @@ const renderCartItems = () => {
 
   cartListEl.innerHTML = markup;
   addListenerToDeleteBtns();
+  addListenersToChangeQuantityBtns();
 };
 
 const onProductListClick = (e) => {
@@ -92,6 +171,7 @@ const onProductListClick = (e) => {
 
   addProductToLS(productData);
   renderCartItems();
+  countTotalCost();
 };
 
 const onDelete = (e) => {
@@ -99,8 +179,10 @@ const onDelete = (e) => {
 
   removeProductFromLS(productId);
   renderCartItems();
+  countTotalCost();
 };
 
 productListEl.addEventListener("click", onProductListClick);
 
 renderCartItems();
+countTotalCost();

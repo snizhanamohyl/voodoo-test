ef8b04cc90f21039cb21c7b86403a2acb067fb92
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-16 18:56:12 +0300

    add: error msg in case of no products

--- src/js/render-products.js
+++ src/js/render-products.js
@@ -3,11 +3,22 @@ import { createProductListMarkup } from "./markup-function";
 
 const paginationEl = document.getElementById("pagination");
 const cardListEl = document.getElementById("card-list");
+const errorMsgEl = document.getElementById("error-msg");
 
 export const renderList = async (page) => {
   paginationEl.classList.add("pointer-events-none");
 
   const products = await getAllProducts(page);
+
+  if (!products) {
+    cardListEl.classList.add("hidden");
+    paginationEl.classList.add("hidden");
+    errorMsgEl.classList.remove("hidden");
+  } else {
+    cardListEl.classList.remove("hidden");
+    paginationEl.classList.remove("hidden");
+    errorMsgEl.classList.add("hidden");
+  }
 
   const markup = createProductListMarkup(products);
   cardListEl.innerHTML = markup;
